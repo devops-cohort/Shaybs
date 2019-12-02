defeb7438116f84007b56e3c9bca2d281928b130
Shaybs <ucashus@ucl.ac.uk>
2019-12-02 18:03:23 +0000

    fixed error in login form

--- application/routes.py
+++ application/routes.py
@@ -64,7 +64,7 @@ def login():
                     if next_page:
                             return redirect(next_page)
                     else:
-                            return redirect(url_for('/home'))
+                            return redirect(url_for('home'))
                     
     return render_template('login.html', title='Login', form=form)
 
