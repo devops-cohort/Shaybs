76615b65e4d2a1dd2173ed61d2e41ee2913fde49
Shaybs <ucashus@ucl.ac.uk>
2019-12-02 14:11:43 +0000

    Changes to init file

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("USERNAME")+":"+os.getenv("PASSWORD")+"@35.197.196.36/bookapp"
 app.config['SECRET_KEY'] = 'hjd9848rujjuuIJDF84Uj*(&ye&*£KMCD938JUud(K'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from application import routes
