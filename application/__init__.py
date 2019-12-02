592644ba28ed54e4e5be316a11665565580f9b80
Shaybs <ucashus@ucl.ac.uk>
2019-12-02 16:13:29 +0000

    Minor changes

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
-from flas_login import LoginManager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
