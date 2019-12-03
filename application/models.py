76d61ee8d419082f447a633272f839f57bd5e205
Shaybs <ucashus@ucl.ac.uk>
2019-12-03 10:28:49 +0000

    Fixes to the models.py

--- application/models.py
+++ application/models.py
@@ -30,7 +30,7 @@ class Book_Posts(db.Model):
 
 class Users(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
-        firt_name = db.Column(db.String(50), nullable=False)
+        first_name = db.Column(db.String(50), nullable=False)
         last_name = db.Column(db.String(50), nullable=False)
         email = db.Column(db.String(250), nullable=False, unique=True)
         password = db.Column(db.String(500), nullable=False)
@@ -38,6 +38,6 @@ class Users(db.Model, UserMixin):
         
         def __repr__(self):
                 return ''.join(['User ID: ', str(self.id), '\r\n',
-                                'Email: ', self.email], '\r\n',
-                               'Name: ', self.first_name, ' ', self.last_name)
+                                'Email: ', self.email, '\r\n',
+                               'Name: ', self.first_name, ' ', self.last_name])
                 
