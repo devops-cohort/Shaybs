27c5c9a69d6f7cfd2b79e1b73600c577a67888ef
Shaybs <ucashus@ucl.ac.uk>
2019-12-03 12:42:52 +0000

    Working update

--- application/routes.py
+++ application/routes.py
@@ -114,18 +114,19 @@ def post():
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-        form = UpdateAccountForm()
-<<<<<<< HEAD
-        if form.validate_on_submit():
-                current_user.first_name = form.first_name.data
-                current_user.last_name = form.last_name.data
-                current_user.email = form.email.data
-                db.session.commit()
-                return redirect(url_for('account'))
-        elif request.method == 'GET'
-                form.first_name.data = current_user.first_name
-                form.last_name.data = current_user.last_name
-                form.email.data = current_user.email                                
+	form = UpdateAccountForm()
+
+	if form.validate_on_submit():
+		current_user.first_name = form.first_name.data
+		current_user.last_name = form.last_name.data
+		current_user.email = form.email.data
+		db.session.commit()
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.first_name.data = current_user.first_name
+		form.last_name.data = current_user.last_name
+		form.email.data = current_user.email
+	return render_template('account.html', title='Account', form=form)
 
 dummyData = [
 	{
