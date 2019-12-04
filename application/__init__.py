730506abd69a8a618a1520eab92e4cc2fd8fba07
Shaybs <ucashus@ucl.ac.uk>
2019-12-04 12:51:32 +0000

    Installed gunicorn and added it to requirements.txt

--- application/__init__.py
+++ application/__init__.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("USERNAME")+":"+os.getenv("PASSWORD")+"@35.197.196.36/bookapp"
-app.config['SECRET_KEY'] = 'hjd9848rujjuuIJDF84Uj*(&ye&*£KMCD938JUud(K'
+app.config['SECRET_KEY'] = 'hjd9848rujjuuIJDF84UjyeKMCD938JUudK'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
