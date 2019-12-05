e7a866385ab8d5b918b6b4d32c7fea99f5592ae7
Shaybs <ucashus@ucl.ac.uk>
2019-12-05 16:34:33 +0000

    Making Front End changes

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from flask import abort, render_template, redirect, url_for, request, flash
 from application import app, db, bcrypt, login_manager
-from application.models import Posts, Books, Users, Reviews
+from application.models import Books, Users, Reviews
 from application.forms import PostForm, Book_PostForm, RegistrationForm, UpdateAccountForm, LoginForm, ReviewForm
 from flask_login import login_user, current_user, logout_user, login_required
 
