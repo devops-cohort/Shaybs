93196c767926ba34ba15137989c6a4f2a6aa03a0
Shaybs <ucashus@ucl.ac.uk>
2019-12-06 21:38:53 +0000

    Adding tests

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -162,29 +162,29 @@ class TestDelete(TestBase):
 		self.assertNotEqual(review[0].review, "Interesting book")
 		self.assertNotEqual(review[0].rating, "5")
 
+
+#	def test_update_review(self):
+
 class Login(TestBase):
-	def test_update_review(self):
 
     # Ensure that main page requires user login
-    def test_book_route_requires_login(self):
-        response = self.client.get('/', follow_redirects=True)
-        self.assertIn(b'Login', response.data)
+	def test_book_route_requires_login(self):
+		response = self.client.get('/', follow_redirects=True)
+		self.assertIn(b'Login', response.data)
 
     # Ensure that welcome page loads
-    def test_welcome_route_works_as_expected(self):
-        response = self.client.get('/Home', follow_redirects=True)
-        self.assertIn(b'Home Page!', response.data)
+	def test_welcome_route_works_as_expected(self):
+		response = self.client.get('/Home', follow_redirects=True)
+		self.assertIn(b'Home Page!', response.data)
 
     # Ensure that posts show up on the main page
-    def test_posts_show_up_on_main_page(self):
-        response = self.client.post(
-            '/login',
-            data=dict(email="admin@admin.com", password="admin2016"),
-            follow_redirects=True
-        )
-        self.assertIn(b"Welcome to Shuaib's Book Web Application!", response.data)
+	def test_posts_show_up_on_main_page(self):
+		response = self.client.post(
+			'/login',
+			data=dict(email="admin@admin.com", password="admin2016"),
+			follow_redirects=True
+		)
+		self.assertIn(b"Welcome to Shuaib's Book Web Application!", response.data)
 
 if __name__ == '__main__':
-    unittest.main()
-
-
+	unittest.main()
