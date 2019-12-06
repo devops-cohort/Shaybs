cb55d903f814bc6f8a3b2e1f32876b3e81184183
Shaybs <ucashus@ucl.ac.uk>
2019-12-06 21:51:31 +0000

    Adding tests

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -175,7 +175,7 @@ class Login(TestBase):
     # Ensure that welcome page loads
 	def test_home_route_works_as_expected(self):
 		response = self.client.get(url_for('books'), follow_redirects=True)
-		self.assertIn(b"Welcome to Shuaib's Book Web Application!", response.data)
+		self.assertIn(b"Book Application", response.data)
 
     # Ensure that posts show up on the main page
 	def test_posts_show_up_on_main_page(self):
@@ -184,7 +184,7 @@ class Login(TestBase):
 			data=dict(email="admin@admin.com", password="admin2016"),
 			follow_redirects=True
 		)
-		self.assertIn(b"Welcome to Shuaib's Book Web Application!", response.data)
+		self.assertIn(b"Book Application", response.data)
 
 if __name__ == '__main__':
 	unittest.main()
