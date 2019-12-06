c83d43faa9f873833adbc3be13b593c460d4f0bc
Shaybs <ucashus@ucl.ac.uk>
2019-12-06 21:44:49 +0000

    Adding tests

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -169,13 +169,13 @@ class Login(TestBase):
 
     # Ensure that main page requires user login
 	def test_book_route_requires_login(self):
-		response = self.client.get('/', follow_redirects=True)
+		response = self.client.get(url_for('books'), follow_redirects=True)
 		self.assertIn(b'Login', response.data)
 
     # Ensure that welcome page loads
-	def test_welcome_route_works_as_expected(self):
+	def test_home_route_works_as_expected(self):
 		response = self.client.get('/Home', follow_redirects=True)
-		self.assertIn('Home Page!', response.data)
+		self.assertIn(b'Home Page', response.data)
 
     # Ensure that posts show up on the main page
 	def test_posts_show_up_on_main_page(self):
