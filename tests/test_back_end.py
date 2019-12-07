79bb9f3db69a90eb88b1b6ae9a3c7a20e1be1306
Shaybs <ucashus@ucl.ac.uk>
2019-12-07 01:32:59 +0000

    Added Delete Testing functionality

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -193,7 +193,7 @@ class ModelTests(TestBase):
 		db.session.commit()
 		self.assertEqual(Reviews.query.count(), 1)
 
-	def test_reviews_delete_model(self):
+	def test_books_delete_model(self):
 		Books.query.filter(Reviews.id == 1).delete()
 		db.session.commit()
 		self.assertEqual(Books.query.count(), 1)
