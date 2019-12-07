45832fce12ae0579241fd9cda6c59d3a6d404f93
Shaybs <ucashus@ucl.ac.uk>
2019-12-07 03:14:12 +0000

    Adding the second last variable for increased security

--- application/__init__.py
+++ application/__init__.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("USERNAME")+":"+os.getenv("PASSWORD")+"@"+os.getenv("MYSQL_URL")+"/"+os.getenv("MYSQL_DB")
-app.config['SECRET_KEY'] = 'hjd9848rujjuuIJDF84UjyeKMCD938JUudK'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
