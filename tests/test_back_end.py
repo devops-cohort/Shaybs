2766be7c4b7e7fc60f49d7cbf92e59fc476d31a8
Shaybs <ucashus@ucl.ac.uk>
2019-12-07 01:11:08 +0000

    Adding delete functionality tests

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -183,6 +183,23 @@ class ModelTests(TestBase):
 		db.session.commit()
 		self.assertEqual(Reviews.query.count(), 3)
 
+	def test_users_delete_model(self):
+		piers = Users(id=3)
+		db.session.delete(piers)
+		db.session.commit()
+		self.assertEqual(Users.query.count(), 2)
+
+	def test_books_delete_model(self):
+		effectiveExecutive = Reviews(id=3)
+		db.session.delete(effectiveExecutive)
+		db.session.commit()
+		self.assertEqual(Books.query.count(), 2)
+
+	def test_reviews_delete_model(self):
+		dmytroReview = Reviews(id=3)
+		db.session.delete(dmytroReview)
+		db.session.commit()
+		self.assertEqual(Reviews.query.count(), 2)
 
 
 class TestLogin(TestBase):
