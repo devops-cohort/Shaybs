9447f16b3417019e51daac2290e1a54a8a94df18
Shaybs <ucashus@ucl.ac.uk>
2020-01-22 15:27:15 +0000

    Adding X-Ray API Call data collection feature

--- FrontEnd/application/__init__.py
+++ FrontEnd/application/__init__.py
@@ -4,6 +4,8 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__)
 
@@ -14,5 +16,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 bootstrap = Bootstrap(app)
+xray_recorder.configure(service='my_app_name')
+XRayMiddleware(app, xray_recorder)
 
 from application import routes
